f2846681e2a6a645c4911e0749ca727ce3937037
hondajohn88 <hondajohn88@gmail.com>
2018-08-10 21:47:49 -0700

    Changes for treble - WIP.

--- sepolicy.mk
+++ sepolicy.mk
@@ -1,9 +1,21 @@
 # Board specific SELinux policy variable definitions
-BOARD_SEPOLICY_DIRS := \
-       $(BOARD_SEPOLICY_DIRS) \
-       device/qcom/sepolicy \
-       device/qcom/sepolicy/common \
-       device/qcom/sepolicy/test \
-       device/qcom/sepolicy/$(TARGET_BOARD_PLATFORM)
+BOARD_SEPOLICY_DIRS += \
+    device/qcom/sepolicy/common \
+    device/qcom/sepolicy/ssg \
+    device/qcom/sepolicy/$(TARGET_BOARD_PLATFORM)
 
--include vendor/crdroid/sepolicy/qcom/sepolicy.mk
+ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+BOARD_SEPOLICY_DIRS += \
+    device/qcom/sepolicy/test
+endif
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/qcom/sepolicy/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/qcom/sepolicy/private
+
+# Add sepolicy version to support OS upgrade and backward compatibility
+BOARD_SEPOLICY_VERS := $(PLATFORM_SDK_VERSION).0
+
+-include device/lineage/sepolicy/qcom/sepolicy.mk
