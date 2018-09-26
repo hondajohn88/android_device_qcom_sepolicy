2d280c3501b678f5f997e01dc8346ebbfb993bbb
hondajohn88 <hondajohn88@gmail.com>
2018-09-25 17:33:31 -0700

    Kang wloot's qcom/sepolicy, and made a few changes.

--- sepolicy.mk
+++ sepolicy.mk
@@ -1,12 +1,25 @@
 # Board specific SELinux policy variable definitions
+LOCAL_PATH:= $(call my-dir)
+
 BOARD_SEPOLICY_DIRS += \
-    device/qcom/sepolicy/common \
-    device/qcom/sepolicy/ssg \
-    device/qcom/sepolicy/$(TARGET_BOARD_PLATFORM)
+       device/qcom/sepolicy/vendor/common \
+       device/qcom/sepolicy/vendor/ssg \
+       device/qcom/sepolicy/vendor/common/sysmonapp
+
+ifeq ($(TARGET_SEPOLICY_DIR),)
+BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/vendor/$(TARGET_BOARD_PLATFORM)
+else
+BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/vendor/$(TARGET_SEPOLICY_DIR)
+endif
 
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 BOARD_SEPOLICY_DIRS += \
-    device/qcom/sepolicy/test
+    device/qcom/sepolicy/vendor/test
+endif
+
+ifneq (,$(filter 24 25 26 27, $(PRODUCT_SHIPPING_API_LEVEL)))
+BOARD_SEPOLICY_DIRS += \
+       device/qcom/sepolicy/vendor/ota/$(TARGET_BOARD_PLATFORM)
 endif
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
@@ -14,8 +27,3 @@ BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/qcom/sepolicy/private
-
-# Add sepolicy version to support OS upgrade and backward compatibility
-BOARD_SEPOLICY_VERS := $(PLATFORM_SDK_VERSION).0
-
--include device/lineage/sepolicy/qcom/sepolicy.mk
